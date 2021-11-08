0d9a59ec1a37b3f15aa9bdeda0ac6618318439e7
phuongvo9 <huyphuong4116@gmail.com>
2021-11-08 20:56:19 +0700

    add docker file

---
+++ Manage-deploy-GKE/container-build-run.sh
@@ -0,0 +1,57 @@
+# build, run, and debug Docker containers.
+# pull Docker images from Docker Hub and Google Container Registry.
+# push Docker images to Google Container Registry.
+
+docker run hello-world
+
+docker images
+
+docker ps
+
+mkdir test && cd test
+
+##################################################
+### BUILD
+##################################################
+# Create a docker file
+cat > Dockerfile <<EOF
+# Use an official Node runtime as the parent image
+FROM node:6
+# Set the working directory in the container to /app
+WORKDIR /app
+# Copy the current directory contents into the container at /app
+ADD . /app
+# Make the container's port 80 available to the outside world
+EXPOSE 80
+# Run app.js using node when the container launches
+CMD ["node", "app.js"]
+EOF
+
+
+# Create a node application
+cat > app.js <<EOF
+const http = require('http');
+const hostname = '0.0.0.0';
+const port = 80;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World\n');
+});
+server.listen(port, hostname, () => {
+    console.log('Server running at http://%s:%s/', hostname, port);
+});
+process.on('SIGINT', function() {
+    console.log('Caught interrupt signal and will exit');
+    process.exit();
+});
+EOF
+
+# The -t is to name and tag an image with the name:tag syntax. The name of the image is node-app and the tag is 0.1. The tag is highly recommended when building Docker images. If you don't specify a tag, the tag will default to latest and it becomes more difficult to distinguish newer images from older ones. Also notice how each line in the Dockerfile above results in intermediate container layers as the image is built.
+
+docker images
+
+
+##################################################
+### RUN
+##################################################
