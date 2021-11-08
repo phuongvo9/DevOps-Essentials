59a694175db4c3d5bf379c09b81a555cad6625b4
phuongvo9 <huyphuong4116@gmail.com>
2021-11-08 21:16:19 +0700

    update container file

--- Manage-deploy-GKE/container-build-run.sh
+++ Manage-deploy-GKE/container-build-run.sh
@@ -55,3 +55,104 @@ docker images
 ##################################################
 ### RUN
 ##################################################
+
+# Run container based on the image
+
+docker run -p 4000:80 --name my-app node-app:0.1
+
+# Check the process is listening
+curl http://localhost:4000
+
+# Stop and remove container
+docker stop my-app && docker rm my-app
+
+# Start container from background -d
+docker run -p 4000:80 --name my-app -d node-app:0.1
+docker ps
+
+# check logs
+docker logs [container_id]
+
+# BUILD NEW IMAGE WITH TAGS 0.2
+nano app.js
+    # ....
+    # const server = http.createServer((req, res) => {
+    #     res.statusCode = 200;
+    #     res.setHeader('Content-Type', 'text/plain');
+    #         res.end('Welcome to Cloud\n');
+    # });
+    # ....
+
+docker build -t node-app:0.2 .
+
+docker images
+
+docker run -p 8080:80 --name my-app-2 -d node-app:0.2
+docker ps
+
+curl http://localhost:8080
+
+#Output: Welcome to Cloud
+
+##################################################
+### DEBUG
+##################################################
+
+#  logs of a container using docker logs [container_id]. If you want to follow the log's output as the container is running, use the -f
+
+docker logs -f [container_id]
+
+# Sometimes you will want to start an interactive Bash session inside the running container. You can use docker exec to do
+
+docker exec -it [container_id] bash  
+    ls
+    exit
+# You can examine a container's metadata in Docker by using Docker inspec
+docker inspect [container_id]
+
+# Use --format to inspect specific fields from the returned JSON
+
+docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' [container_id]
+# Output: 192.168.9.3
+
+##################################################
+### PUBLISH
+##################################################
+
+#  push the image to the Google Container Registry (gcr).
+gcloud config list project
+    #Tag node-app:0.2. Replace [project-id] with your configuration..
+        # format is [hostname]/[project-id]/[image]:[tag].
+docker tag node-app:0.2 gcr.io/[project-id]/node-app:0.2
+
+docker tag node-app:0.2 gcr.io/qwiklabs-gcp-03-0192380e21ba/node-app:0.2
+
+# Push this image to gcr. 
+
+docker push gcr.io/[project-id]/node-app:0.2
+docker push gcr.io/qwiklabs-gcp-03-0192380e21ba/node-app:0.2
+
+
+# Stop and remove all containers:
+
+docker stop $(docker ps -q)
+docker rm $(docker ps -aq)
+
+#  have to remove the child images (of node:6) before we remove the node image - new fresh environment
+
+docker rmi node-app:0.2 gcr.io/qwiklabs-gcp-03-0192380e21ba/node-app node-app:0.1
+docker rmi node:6
+docker rmi $(docker images -aq) # remove remaining images
+docker images
+
+
+# At this point you should have a pseudo-fresh environment. 
+    # Pull the image and run
+
+docker pull gcr.io/[project-id]/node-app:0.2
+docker run -p 4000:80 -d gcr.io/qwiklabs-gcp-03-0192380e21ba/node-app:0.2
+curl http://localhost:4000
+
+
+
+
