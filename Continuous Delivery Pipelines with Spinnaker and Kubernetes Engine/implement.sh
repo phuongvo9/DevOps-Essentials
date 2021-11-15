27b9f136a72f0d2be745bcd447dac09d757a592c
phuongvo9 <huyphuong4116@gmail.com>
2021-11-15 20:56:33 +0700

    Deploying Spinnaker using Helm

--- Continuous Delivery Pipelines with Spinnaker and Kubernetes Engine/implement.sh
+++ Continuous Delivery Pipelines with Spinnaker and Kubernetes Engine/implement.sh
@@ -60,4 +60,18 @@ export SA_EMAIL=$(gcloud iam service-accounts list \
 gcloud beta pubsub subscriptions add-iam-policy-binding gcr-triggers \
     --role roles/pubsub.subscriber --member serviceAccount:$SA_EMAIL
 
-    
+############################################
+#### Deploying Spinnaker using Helm
+############################################
+
+# Configure Helm
+kubectl create clusterrolebinding user-admin-binding \
+    --clusterrole=cluster-admin --user=$(gcloud config get-value account)
+# Grant Spinnaker the cluster-admin role so it can deploy resources across all namespaces
+kubectl create clusterrolebinding --clusterrole=cluster-admin \
+    --serviceaccount=default:default spinnaker-admin
+
+
+# Add the stable charts deployments to Helm's usable repositories (includes Spinnaker)
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
