943823f8f4dbe49b3c646dbaf0e6dd94068652c5
phuongvo9 <huyphuong4116@gmail.com>
2021-11-15 20:58:42 +0700

    Configure Spinnaker

--- Continuous Delivery Pipelines with Spinnaker and Kubernetes Engine/implement.sh
+++ Continuous Delivery Pipelines with Spinnaker and Kubernetes Engine/implement.sh
@@ -75,3 +75,61 @@ kubectl create clusterrolebinding --clusterrole=cluster-admin \
 # Add the stable charts deployments to Helm's usable repositories (includes Spinnaker)
 helm repo add stable https://charts.helm.sh/stable
 helm repo update
+
+
+
+############################################
+#### Configure Spinnaker
+############################################
+
+    #create a bucket for Spinnaker to store its pipeline configuration
+export PROJECT=$(gcloud info \
+    --format='value(config.project)')
+
+
+export BUCKET=$PROJECT-spinnaker-config
+
+gsutil mb -c regional -l us-central1 gs://$BUCKET
+
+
+#  create a spinnaker-config.yaml 
+
+export SA_JSON=$(cat spinnaker-sa.json)
+export PROJECT=$(gcloud info --format='value(config.project)')
+export BUCKET=$PROJECT-spinnaker-config
+cat > spinnaker-config.yaml <<EOF
+gcs:
+  enabled: true
+  bucket: $BUCKET
+  project: $PROJECT
+  jsonKey: '$SA_JSON'
+dockerRegistries:
+- name: gcr
+  address: https://gcr.io
+  username: _json_key
+  password: '$SA_JSON'
+  email: 1234@5678.com
+# Disable minio as the default storage backend
+minio:
+  enabled: false
+# Configure Spinnaker to enable GCP services
+halyard:
+  spinnakerVersion: 1.19.4
+  image:
+    repository: us-docker.pkg.dev/spinnaker-community/docker/halyard
+    tag: 1.32.0
+    pullSecrets: []
+  additionalScripts:
+    create: true
+    data:
+      enable_gcs_artifacts.sh: |-
+        \$HAL_COMMAND config artifact gcs account add gcs-$PROJECT --json-path /opt/gcs/key.json
+        \$HAL_COMMAND config artifact gcs enable
+      enable_pubsub_triggers.sh: |-
+        \$HAL_COMMAND config pubsub google enable
+        \$HAL_COMMAND config pubsub google subscription add gcr-triggers \
+          --subscription-name gcr-triggers \
+          --json-path /opt/gcs/key.json \
+          --project $PROJECT \
+          --message-format GCR
+EOF
