b2a0983dc3dafe088e9b2435be2cd26c91fa5e4b
phuongvo9 <huyphuong4116@gmail.com>
2021-11-15 20:51:27 +0700

    Configure identity and access management

--- Continuous Delivery Pipelines with Spinnaker and Kubernetes Engine/implement.sh
+++ Continuous Delivery Pipelines with Spinnaker and Kubernetes Engine/implement.sh
@@ -13,3 +13,29 @@
 # Deploy a code change, triggering the pipeline, and watch it roll out to production.
 
 
+
+
+gcloud config set compute/zone us-central1-f
+# Create GKE
+gcloud container clusters create spinnaker-tutorial \
+    --machine-type=n1-standard-2
+### Configure identity and access management
+
+# Create service account
+gcloud iam service-accounts create spinnaker-account \
+    --display-name spinnaker-account
+
+# Store the service account email address and the current project ID in environment variables
+export SA_EMAIL=$(gcloud iam service-accounts list \
+    --filter="displayName:spinnaker-account" \
+    --format='value(email)')
+export PROJECT=$(gcloud info --format='value(config.project)')
+
+# Bind the storage.admin role to the service account:
+gcloud projects add-iam-policy-binding $PROJECT \
+    --role roles/storage.admin \
+    --member serviceAccount:$SA_EMAIL
+
+# Download the service account key to install Spinnaker and upload key to GKE
+gcloud iam service-accounts keys create spinnaker-sa.json \
+     --iam-account $SA_EMAIL
