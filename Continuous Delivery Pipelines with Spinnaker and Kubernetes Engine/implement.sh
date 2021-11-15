a103d6f1d8d6046c5cc93ad580275d9f5726c697
phuongvo9 <huyphuong4116@gmail.com>
2021-11-15 20:48:10 +0700

    add file implement CD with Spinnaker K8S

---
+++ Continuous Delivery Pipelines with Spinnaker and Kubernetes Engine/implement.sh
@@ -0,0 +1,15 @@
+# Set up your environment by launching Google Cloud Shell, creating a Kubernetes Engine cluster, and configuring your identity and user management scheme.
+
+# Download a sample application, create a Git repository then upload it to a Google Cloud Source Repository.
+
+# Deploy Spinnaker to Kubernetes Engine using Helm.
+
+# Build your Docker image.
+
+# Create triggers to create Docker images when your application changes.
+
+# Configure a Spinnaker pipeline to reliably and continuously deploy your application to Kubernetes Engine.
+
+# Deploy a code change, triggering the pipeline, and watch it roll out to production.
+
+
