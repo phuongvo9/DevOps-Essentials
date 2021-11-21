c78e103d41e03d1055c574cbec4aa390d1d4f501
phuongvo9 <huyphuong4116@gmail.com>
2021-11-21 22:12:00 +0700

    Building Containers with a build configuration file and Cloud Build

--- GCP-CloudBuild/WorkwithCloudbuild.sh
+++ GCP-CloudBuild/WorkwithCloudbuild.sh
@@ -15,3 +15,7 @@ chmod +x quickstart.sh
 # build the Docker container image in Cloud Build.
 
 gcloud builds submit --tag gcr.io/${GOOGLE_CLOUD_PROJECT}/quickstart-image .
+
+
+### Building Containers with a build configuration file and Cloud Build
+
