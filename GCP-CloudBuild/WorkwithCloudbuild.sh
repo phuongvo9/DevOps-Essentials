62d647604dd63a724c7fc9695c5aa54308f552de
phuongvo9 <huyphuong4116@gmail.com>
2021-11-21 22:13:13 +0700

    start a Cloud Build using cloudbuild.yaml as the build configuration file

--- GCP-CloudBuild/WorkwithCloudbuild.sh
+++ GCP-CloudBuild/WorkwithCloudbuild.sh
@@ -19,3 +19,22 @@ gcloud builds submit --tag gcr.io/${GOOGLE_CLOUD_PROJECT}/quickstart-image .
 
 ### Building Containers with a build configuration file and Cloud Build
 
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+
+# Create a soft link as a shortcut to the working directory
+ln -s ~/training-data-analyst/courses/ak8s/v1.1 ~/ak8s
+
+cd ~/ak8s/Cloud_Build/a
+
+cat cloudbuild.yaml
+
+# steps:
+# - name: 'gcr.io/cloud-builders/docker'
+#   args: [ 'build', '-t', 'gcr.io/$PROJECT_ID/quickstart-image', '.' ]
+# images:
+# - 'gcr.io/$PROJECT_ID/quickstart-image'
+
+# tag it with gcr.io/$PROJECT_ID/quickstart-image
+
+#start a Cloud Build using cloudbuild.yaml as the build configuration file
+gcloud builds submit --config cloudbuild.yaml .
