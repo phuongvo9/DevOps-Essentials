3826fbd7a4b902d9c88b8832265408b1e3e7fd66
phuongvohuy9 <huyphuong4116@gmail.com>
2022-01-06 21:37:30 +0700

    imperative deployment creation

--- Worksheet-K8S/generate-YAML.sh
+++ Worksheet-K8S/generate-YAML.sh
@@ -1,6 +1,14 @@
 # Create nginx POD
 kubectl run nginx-pod --image=nginx
 
-# we can get away with just the kubectl run command without having to create a YAML file at all
+    # -- we can get away with just the kubectl run command without having to create a YAML file at all
 
+# Generate POD manifest yaml without creaing a pod
 kubectl run nginx --image=nginx --dry-run=client -o yaml
+
+
+# Create deployment
+kubectl create deployment nginx --image=nginx
+kubectl get deployment
+
+
