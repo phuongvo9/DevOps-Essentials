44bb89660643bd7560b2a904b51fa79f79ba5128
phuongvohuy9 <huyphuong4116@gmail.com>
2022-01-06 21:36:05 +0700

    --dry-run=client generate yaml

--- Worksheet-K8S/generate-YAML.sh
+++ Worksheet-K8S/generate-YAML.sh
@@ -1,2 +1,6 @@
 # Create nginx POD
 kubectl run nginx-pod --image=nginx
+
+# we can get away with just the kubectl run command without having to create a YAML file at all
+
+kubectl run nginx --image=nginx --dry-run=client -o yaml
