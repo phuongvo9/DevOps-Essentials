b7abef3f26eba60ed60d42b1a1bc6fa2c346a768
Phuong Vo Huy <phuong.voh@homecredit.vn>
2022-03-19 22:00:07 +0700

    feat: deploy blank template

--- Azure-ARMtemplate/Local-ARMtemplate/README.md
+++ Azure-ARMtemplate/Local-ARMtemplate/README.md
@@ -1,2 +1,8 @@
 # Sandbox - Microsoft Azure
 https://docs.microsoft.com/en-us/learn/modules/create-azure-resource-manager-template-vs-code/3-exercise-create-and-deploy-template?pivots=powershell
+
+
+## Documents
+### Deploy Local ARM template with Azure CLI
+https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/deploy-cli?toc=%2Fcli%2Fazure%2Ftoc.json&bc=%2Fcli%2Fazure%2Fbreadcrumb%2Ftoc.json&view=azure-cli-latest
+
---
+++ Azure-ARMtemplate/Local-ARMtemplate/az-cmds.sh
@@ -0,0 +1,30 @@
+az login
+
+az account subscription list
+
+# Create a Resource group
+
+location='WestUS'
+az group create \
+  --name {name of your resource group} \
+  --location "$location"
+
+
+ 
+$templateFile = "azuredeploy.json"
+$today=Get-Date -Format "MM-dd-yyyy"
+$deploymentName="blanktemplate-"+"$today"
+# New-AzResourceGroupDeployment
+#   -Name $deploymentName `
+#   -TemplateFile $templateFile
+
+my_resource_group='learn-bd92f45e-7000-4712-b9be-1ae12bd1ed9c'
+deploymentName='blanktemplate-19032022'
+templateFile='azuredeploy.json'
+# To deploy to a resource group
+
+az deployment group create \
+  --name $deploymentName \
+  --resource-group $my_resource_group \
+  --template-file $templateFile \
+ # --parameters storageAccountType=Standard_GRS
