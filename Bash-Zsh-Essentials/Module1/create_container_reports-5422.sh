0bdf8611036d39d8bdc8ab17a602bd1f383acd7e
phuongvo9 <huyphuong4116@gmail.com>
2022-04-05 22:01:22 +0700

    chore: mkdir and grep

--- Bash-Zsh-Essentials/Module1/create_container_reports-5422.sh
+++ Bash-Zsh-Essentials/Module1/create_container_reports-5422.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-$container="$1"
-$directory="$2"
+
 
 if [[ ! $1 ]]; then
     echo "Error: missing argument - container name"
     exit 1
 fi
+
+if [[ ! $2 ]]; then
+    echo "Error: missing argument - directory"
+    exit 1
+fi
+
+$container="$1"
+$directory="$2"
+
+mkdir -p $directory
+grep -- "$container" ./shipments.csv > "$directory/${container}_reports.csv"
+echo report created
