48b2463437712d816c24432a4199b646f098c5c2
phuongvo9 <huyphuong4116@gmail.com>
2022-04-24 11:54:46 +0700

    basic if validation

--- Bash-Zsh-Essentials/Conditions/condition-0.sh
+++ Bash-Zsh-Essentials/Conditions/condition-0.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo hello && echo goodbye
+#echo hello && echo goodbye
+
+#echo hello || echo goodbye
+
+declare -i days
+read days
+
+if [ $days -lt 1 ] && [ $days -gt 31 ]; then
+    echo "Please enter correct value"
+else echo "Your value is acceptable"
+fi
