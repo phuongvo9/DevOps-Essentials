695f56c88f01908fc2a51b5aad9276a773104ace
phuongvo9 <huyphuong4116@gmail.com>
2022-06-19 16:45:41 +0700

    init: write_command_for_shell_scripts.sh

---
+++ Bash-Zsh-Essentials/Heredocs-Herestrings/write_command_for_shell_scripts.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This is the invididual command
+
+<< MYCOMMENT
+This is multi line comments
+Using this type of comment to write many lines
+MYCOMMENT
+
+tr [a-z] [A-Z] <<< "I show you how to comments in shell scripts"
