4d031260a866744cbb3c0e6809ebe249a96a88af
phuongvo9 <huyphuong4116@gmail.com>
2022-06-21 23:11:05 +0700

    update: chain_commands.sh

--- Bash-Zsh-Essentials/Conditions/chain_commands.sh
+++ Bash-Zsh-Essentials/Conditions/chain_commands.sh
@@ -3,7 +3,9 @@
 # Command chaining operators: ; && ||
 # cmd1 ; cmd2 -- cmd2 run whether cmd1 success or not
 # cmd1 && cmd2 -- cmd2 only run after cmd1's success
-# cmd1 || cmd2 -- cmd1's success or cmd'2 success?
+# cmd1 || cmd2 -- cmd2 only run if cmd1's failed
 
-
+echo "Hello" ; echo "We did say hello"
 which apache2 && apache2 -v
+
+which docker && echo "docker is installed" || echo "docker is not installed"
