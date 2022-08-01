e1bc86793d167b15aa928e19c2508fa015b2d4cd
phuongvo9 <huyphuong4116@gmail.com>
2022-08-01 18:30:51 +0700

    transform (limit funtion return array)

--- Bash-Zsh-Essentials/jq-practice/jq-2/jq-2-query0.sh
+++ Bash-Zsh-Essentials/jq-practice/jq-2/jq-2-query0.sh
@@ -6,3 +6,29 @@ cat openlib.json | jq .docs[] | jq .title
 
 cat openlib.json | jq ".docs[] | {title, author_name, publish_year}"
 cat openlib.json | jq ".docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]}"
+
+# select function
+cat openlib.json | jq ".docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+                        | select ( .publish_year != null and .author_name != null)"
+
+# sort_by function
+cat openlib.json | jq ".docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+    | select ( .publish_year != null and .author_name != null) | sort_by (.publish_year)"
+    ## Output: Error - Cannot index string with string "publish_year" - sort_by accepts ARRAY
+# sort_by function - Fixed with array
+cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+    | select ( .publish_year != null and .author_name != null) ] | sort_by (.publish_year)"
+# sort_by function - Desending order
+cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+    | select ( .publish_year != null and .author_name != null) ] | sort_by (.publish_year) | reverse"
+
+# Get Length
+cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+    | select ( .publish_year != null and .author_name != null) ] | sort_by (.publish_year) | reverse | length"
+# limit with expression
+cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+    | select ( .publish_year != null and .author_name != null) ] | sort_by (.publish_year) | reverse | limit(3;.[])"
+
+# transform (limit funtion return array)
+cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+    | select ( .publish_year != null and .author_name != null) ] | sort_by (.publish_year) | reverse | [limit(3;.[])]"
