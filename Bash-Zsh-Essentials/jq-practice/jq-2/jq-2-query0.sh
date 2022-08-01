2b46a52e41b1e831f3a8fd4d441e4500b3ed20b7
phuongvo9 <huyphuong4116@gmail.com>
2022-08-01 22:14:56 +0700

    count sort_by resverse

--- Bash-Zsh-Essentials/jq-practice/jq-2/jq-2-query0.sh
+++ Bash-Zsh-Essentials/jq-practice/jq-2/jq-2-query0.sh
@@ -18,7 +18,7 @@ cat openlib.json | jq ".docs[] | {title, author_name: .author_name[0], publish_y
 # sort_by function - Fixed with array
 cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
     | select ( .publish_year != null and .author_name != null) ] | sort_by (.publish_year)"
-# sort_by function - Desending order
+# sort_by function - Desending order - sort_by (.publish_year)  | reverse
 cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
     | select ( .publish_year != null and .author_name != null) ] | sort_by (.publish_year) | reverse"
 
@@ -32,3 +32,19 @@ cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_
 # transform (limit funtion return array)
 cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
     | select ( .publish_year != null and .author_name != null) ] | sort_by (.publish_year) | reverse | [limit(3;.[])]"
+
+
+# GROUP_BY
+cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+    | select ( .publish_year != null and .author_name != null) ] | group_by (.author_name)"
+
+    # Walk through each array (group_by author) and get the author name via [0]
+        # group_by (.author_name) | .[] | {author_name: .[0].author_name}
+cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+| select ( .publish_year != null and .author_name != null) ] | group_by (.author_name) | .[] | {author_name: .[0].author_name}"
+    # count each author
+cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+| select ( .publish_year != null and .author_name != null) ] | group_by (.author_name) | .[] | {author_name: .[0].author_name, count: . | length}"
+    # produce array to sort with count
+cat openlib.json | jq "[.docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+| select ( .publish_year != null and .author_name != null) ] | group_by (.author_name) | [.[] | {author_name: .[0].author_name, count: . | length}] | sort_by (.count) | reverse | limit(3;.[])"
