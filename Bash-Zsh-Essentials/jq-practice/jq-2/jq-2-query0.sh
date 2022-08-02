401d6264342bd60995088e02ec51795be8377657
phuongvo9 <huyphuong4116@gmail.com>
2022-08-02 22:51:39 +0700

    select in jq

--- Bash-Zsh-Essentials/jq-practice/jq-2/jq-2-query0.sh
+++ Bash-Zsh-Essentials/jq-practice/jq-2/jq-2-query0.sh
@@ -10,6 +10,8 @@ cat openlib.json | jq ".docs[] | {title, author_name: .author_name[0], publish_y
 # select function
 cat openlib.json | jq ".docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
                         | select ( .publish_year != null and .author_name != null)"
+cat openlib.json | jq ".docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
+                        | select ( .publish_year != null and .author_name == null)"
 
 # sort_by function
 cat openlib.json | jq ".docs[] | {title, author_name: .author_name[0], publish_year: .publish_year[0]} \
