6932ad2cc4f53bfbab03460ee338cedc5c5e43c0
phuongvo9 <huyphuong4116@gmail.com>
2022-08-09 11:06:32 +0700

    chore: update

--- Bash-Zsh-Essentials/jq-practice/jq-3/jq-3-query0.sh
+++ Bash-Zsh-Essentials/jq-practice/jq-3/jq-3-query0.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
+# https://stedolan.github.io/jq/tutorial/
+
+
 curl 'https://api.github.com/repos/stedolan/jq/commits?per_page=5' > commit.json
 cat commit.json | jq '.'
+
+
+# get first item
+cat commit.json | jq '.[0]'
+# commit name and message in first item
+cat commit.json | jq '.[0] | {commit_name: .commit.author.name, commit_message: .commit.message}'
+
+# commit name and message in all items (iterate)
+cat commit.json | jq '.[] | {commit_name: .commit.author.name, commit_message: .commit.message}'
+
+# Wrap []: [commit name and message in all items (iterate)]
+cat commit.json | jq '[.[] | {commit_name: .commit.author.name, commit_message: .commit.message}]'
+
+# Wrap []: [commit name and message + parents[] in all items (iterate)]
+cat commit.json | jq '[.[] | {commit_name: .commit.author.name, commit_message: .commit.message, parents: [.parents[].html_url]}]'
