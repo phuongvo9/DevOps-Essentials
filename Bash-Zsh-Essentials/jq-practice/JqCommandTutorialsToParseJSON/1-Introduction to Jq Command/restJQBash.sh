ab122c400e1186dc76751c9398b6460e4def5289
phuongvo9 <huyphuong4116@gmail.com>
2022-08-12 11:49:18 +0700

    update restJQBash

--- Bash-Zsh-Essentials/jq-practice/JqCommandTutorialsToParseJSON/1-Introduction to Jq Command/restJQBash.sh
+++ Bash-Zsh-Essentials/jq-practice/JqCommandTutorialsToParseJSON/1-Introduction to Jq Command/restJQBash.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+<<MYRESPONSE
+data={
+    "secrets": {
+        "remoteServerCreds": {
+            "user": "ec2-user",
+            "pass" : "ec2-user@123"
+        },
+        "awsAccessKeys" : {
+            "aws_access_key_id": "",
+            "aws_secret_access_key": ""
+        }       
+    }
+}
+MYRESPONSE
 
 response=$(curl -s http://192.168.0.110:8080/secrets | jq ".")
 userName=$(echo ${response} | jq -r ".secrets.remoteServerCreds.userName")
