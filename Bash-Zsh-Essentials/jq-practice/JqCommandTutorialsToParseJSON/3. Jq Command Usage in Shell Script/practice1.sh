db2199d515b38945b8dbd0d6900417f265e3bf5d
phuongvo9 <huyphuong4116@gmail.com>
2022-08-12 11:55:49 +0700

    init: edit-contruct object and array

--- Bash-Zsh-Essentials/jq-practice/JqCommandTutorialsToParseJSON/3. Jq Command Usage in Shell Script/practice1.sh
+++ Bash-Zsh-Essentials/jq-practice/JqCommandTutorialsToParseJSON/3. Jq Command Usage in Shell Script/practice1.sh
@@ -3,7 +3,7 @@ hostName=44.204.49.249
 response=$(curl -s http://192.168.0.110:8080/secrets)
 #userName=$(echo ${response} | jq -r .secrets.remoteServerCreds.userName)
 #password=$(echo ${response} | jq -r .secrets.remoteServerCreds.password)
-
+response=$(curl -s http://192.168.0.110:8080/secrets)
 userName=$(jq -r .secrets.remoteServerCreds.userName <<< "$(echo ${response})")
 password=$(jq -r .secrets.remoteServerCreds.password <<< "$(echo ${response})")
 
