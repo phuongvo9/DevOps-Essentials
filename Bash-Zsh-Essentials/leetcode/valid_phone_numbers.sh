c50df600dcddeaa51447c69e0bc02b1e43aa02eb
phuongvo9 <huyphuong4116@gmail.com>
2022-08-27 22:21:37 +0700

    fix: regex ( )

--- Bash-Zsh-Essentials/leetcode/valid_phone_numbers.sh
+++ Bash-Zsh-Essentials/leetcode/valid_phone_numbers.sh
@@ -6,3 +6,17 @@ while read line; do
     fi
 done < "file.txt"
 
+
+#my code
+# Read from the file file.txt and output all valid phone numbers to stdout.
+
+# while read line; do
+#     if [[ "${line}" =~ ^(\([0-9]{3}\)|[0-9]{3})-[0-9]{3}-[0-9]{4}$ ]]; then
+#         echo "${line}"
+#     fi
+# done < "file.txt"
+
+
+^(\([0-9]{3}\) )|[0-9]{3}-[0-9]{3}-[0-9]{4}$
+^(\([0-9]{3}\)|[0-9]{3})-[0-9]{3}-[0-9]{4}$
+^(\([0-9]{3}\)|[0-9]{3})
